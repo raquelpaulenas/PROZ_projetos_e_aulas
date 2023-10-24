14174107e350a9fc9823ad268b310a89d6f61340
raquelpaulenas <raquel.paulenas@gmail.com>
2023-10-24 20:17:49 -0300

    alteração nos arquivos

---
+++ conexao-html-js/script.js
@@ -0,0 +1,20 @@
+const blogTitle = document.getElementById('blog_title')
+console.log (blogTitle)
+
+const post02 = document.getElementById('post02')
+console.log(post02)
+
+const form = document.getElementsByClassName('formulario')
+console.log(form)
+
+const posts = document.getElementsByClassName('post')
+console.log(posts)
+
+const data = document.getElementsByClassName('post-data')
+console.log(data)
+
+const textos = document.getElementsByClassName('post-texto')
+console.log(textos)
+
+const redes = document.getElementsByClassName('lista_redes')
+console.log(redes)
