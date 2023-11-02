1fc893d7c525b102eb83bba57430aa328bad9558
raquelpaulenas <raquel.paulenas@gmail.com>
2023-11-02 20:41:18 -0300

    Alterando class com js

---
+++ aula-style-classList/script.js
@@ -0,0 +1,21 @@
+let titulo = document.querySelector("h2");
+
+titulo.style.color = "blue";
+titulo.style.fontSize = "2.5rem";
+
+
+let button = document.querySelector("button");
+
+
+button.style.borderColor = "blue";
+
+
+
+let loginUsuario  = document.getElementById("login-usuario");
+loginUsuario.classList.add("error")
+let usuarioInvalido = document.querySelector(".error-text")
+usuarioInvalido.classList.add("visible")
+
+
+
+
